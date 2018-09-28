207423c1279fa85b3ca029aaa2bec8f3b117ce25
Alex Smetukh <alex.smetukh@gmail.com>
2018-09-28 21:59:31 +0300

    Add API. server: https://dev-231083-admin.oktapreview.com/dev/console ==> alex.smetukh@gmail.com ==> Doorbell1

---
+++ Environment.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    CLIENT_API: 'dev-231083.oktapreview.com/oauth2/default'
+};
--- src/components/Secured.js
+++ src/components/Secured.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import {
-    ScrollView,
-    Text,
-    View,
-    Button
+	ScrollView,
+	Text,
+	View,
+	Button
 } from 'react-native';
 
 export default class Secured extends Component {
-    render() {
-        return (
-            <ScrollView style={{padding: 20}}>
-                <Text 
-                    style={{fontSize: 27}}>
-                    Welcome
-                </Text>
-                <View style={{margin:20}} />
-                <Button
-                            onPress={this.props.onLogoutPress}
-                            title="Logout"
-                        />
-                </ScrollView>
-                )
-    }
+	render() {
+		return (
+			<ScrollView style={{padding: 20}}>
+				<Text 
+					style={{fontSize: 27}}>
+					Welcome
+				</Text>
+				<View style={{margin:20}} />
+				<Button
+		            onPress={this.props.onLogoutPress}
+		            title="Logout"
+		        />
+		    </ScrollView>
+        )
+	}
 }
+
+
+
+// import React, { Component } from 'react';
+// import {
+//     ScrollView,
+//     Text,
+//     View,
+//     Button
+// } from 'react-native';
+
+// export default class Secured extends Component {
+//     render() {
+//         return (
+//             <ScrollView style={{padding: 20}}>
+//                 <Text 
+//                     style={{fontSize: 27}}>
+//                     Welcome
+//                 </Text>
+//                 <View style={{margin:20}} />
+//                 <Button
+//                             onPress={this.props.onLogoutPress}
+//                             title="Logout"
+//                         />
+//                 </ScrollView>
+//                 )
+//     }
+// }
